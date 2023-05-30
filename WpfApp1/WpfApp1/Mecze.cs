cf1d5bed3aaf6323b74d2277b188631ed14d9019
FireFly0707 <jakubswietliczny@gmail.com>
2023-05-30 21:34:39 +0200

    dokończenie implementacji klasy mecz

--- WpfApp1/WpfApp1/Mecze.cs
+++ WpfApp1/WpfApp1/Mecze.cs
@@ -6,17 +6,47 @@ using System.Threading.Tasks;
 
 namespace WpfApp1
 {
+    /* Mecze w DwaOgnie oraz w PrzeciąganieLiny będą tworzone jako obiekty Mecz*/
     public class Mecz
     {
         static int generatorId = 0;
         public Drużyna Drużyna1 { get; private set; }
         public Drużyna Drużyna2 { get; private set; }
         public Sędzia SędziaGłówny { get; private set; }
-        public string wynik { get; set; }
+
+        // Wynik '0' oznacza że mecz nie został jeszcze rozegrany
+        // Wynik '1' oznacza że wygrała Drużyna1
+        // Wynik '2' oznacza że wygrała Drużyna2
+        
+        public char Wynik { get;  set; }
         public int Id { get; private set; }
-        protected bool Rozegrany = false;
+        
+        public Mecz(Drużyna drużyna1, Drużyna drużyna2, Sędzia sędziaGłówny)
+        {
+            Drużyna1 = drużyna1;
+            Drużyna2 = drużyna2;
+            SędziaGłówny = sędziaGłówny;
+            Wynik = '0';
+        }
+        public bool CzyZakończony()
+        {
+            return Wynik != '0';
+        }
+        
 
 
+    }
+    public class MeczSiatkówki : Mecz
+    {
+        public Sędzia SędziaPomocniczy1 { get; private set; }
+        public Sędzia SędziaPomocniczy2 { get; private set; }
+
+        public MeczSiatkówki(Sędzia sędziaPomocniczy1, Sędzia sędziaPomocniczy2,Drużyna drużyna1, Drużyna drużyna2, Sędzia sędziaGłówny)
+            :base(drużyna1,drużyna2,sędziaGłówny)
+        {
+            SędziaPomocniczy1 = sędziaPomocniczy1;
+            SędziaPomocniczy2 = sędziaPomocniczy2;
+        }
     }
 }
 
