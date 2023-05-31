5f203b65e7b01604289e4a86331069e2e5c0323f
FireFly0707 <jakubswietliczny@gmail.com>
2023-05-31 14:31:47 +0200

    dodanie kolejnych opcji do GUI oraz klasy tabela wyników

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,9 +16,16 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /*Na plaży Kopakabana oprócz plażowania toczą się też rozgrywki w 3 grach zespołowych: siatkówka plażowa,
+     * 2 ognie i przeciąganie liny.Każda z drużyn składa się ze stałej liczby zawodników. 
+     * Toczą oni mecze/spotkania na zasadzie każdy z każdym. Pierwsze 4 drużyny z największą liczbą zwycięstw przechodzą do półfinałów a ich zwycięzcy do finałów.
+     * Każde ze spotkań sędziuje sędzia, a dodatkowo w siatkówce 2 sędziów pomocniczych.
+Minimalny zakres funkcjonalności
+zarządzanie sędziami (dodawanie, usuwanie, przegląd)
+zarządzanie drużynami (zgłaszanie, wycofywanie, przegląd)
+organizacja rozgrywek w każdej dyscyplinie: tworzenie meczów (spotkań), wpisywanie wyników, automatyczne generowanie półfinałów i finałów
+wyświetlanie tabeli wyników
+zapis i odczyt stanu systemu na dysk*/
     public partial class MainWindow : Window
     {
         RejestrSędziów rejestrSędziów = new RejestrSędziów();
@@ -32,6 +40,17 @@ namespace WpfApp1
         private void ZobaczSędziów(object sender, RoutedEventArgs e)
         {
             OknoObsługiSędziów noweOkno = new OknoObsługiSędziów(rejestrSędziów);
+            noweOkno.ShowDialog();
+        }
+
+        private void Zamykanie(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Siatkówka(object sender, RoutedEventArgs e)
+        {
+            OknoObsługiDrużynSiatkówki noweOkno = new OknoObsługiDrużynSiatkówki();
             noweOkno.ShowDialog();
         }
     }
---
+++ WpfApp1/WpfApp1/TabelaWyników.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class WynikDrużyny
+    {
+        public int IdDrużyny { get; private set; }
+        public string NazwaDrużyny { get; private set; }
+        public int Wygrane { get; private set; }
+        public int Porazki { get; private set; }
+        public int Remisy { get; private set; }
+
+    }
+    public class TabelaWyników
+    {
+        public List<WynikDrużyny> Wyniki { get; private set; }
+    }
+}
