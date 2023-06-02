eddd133fa37b453a244e140564993287e79528c4
FireFly0707 <jakubswietliczny@gmail.com>
2023-06-02 09:13:57 +0200

    dodanie klasy Rozgrywki

--- WpfApp1/WpfApp1/Rozgrywki.cs
+++ WpfApp1/WpfApp1/Rozgrywki.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace WpfApp1
 {
-    internal class Rozgrywki
+    public class Rozgrywki
     {
+        public Turniej TurniejSiatkówki { get; private set; }
+        public Turniej TurniejDwaOgnie { get; private set; }
+        public Turniej TurniejPrzeciąganieLiny { get; private set; }
+        public RejestrSędziów Sędziowie {get; private set;}
+        public RejestrDrużyn DrużynySiatkówka { get; private set;}
+        public RejestrDrużyn DrużynyDwaOgnie { get; private set;}
+        public RejestrDrużyn DrużynyPrzeciąganieLiny { get; private set;}
+        public Rozgrywki()
+        {
+            Sędziowie = new RejestrSędziów();
+            DrużynySiatkówka = new RejestrDrużyn();
+            DrużynyDwaOgnie = new RejestrDrużyn();
+            DrużynyPrzeciąganieLiny = new RejestrDrużyn();
+        }
     }
 }
