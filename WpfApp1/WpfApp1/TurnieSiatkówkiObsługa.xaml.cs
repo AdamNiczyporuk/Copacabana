c0b625194c17e9670b93e72d14153e4f283a9a9c
Adam Niczyporuk <85999@student.pb.edu.pl>
2023-06-06 22:15:26 +0200

    dodawnie wisible

--- WpfApp1/WpfApp1/TurnieSiatkówkiObsługa.xaml.cs
+++ WpfApp1/WpfApp1/TurnieSiatkówkiObsługa.xaml.cs
@@ -47,6 +47,7 @@ namespace WpfApp1
             
             ListaMeczy.Items.Refresh();
             ListaWyników.Items.Refresh();
+            wynikMeczu.Visibility = Visibility.Visible;
         }
 
         private void UstawWynikMeczu(object sender, RoutedEventArgs e)
